c1534ca30baf4b4bcad487f42b04c35d52de6c5e
hmkuklinski <hmkuklinski@gmail.com>
2024-09-22 04:03:51 -0400

    added bunny.cpp

---
+++ beginner/bunny.cpp
@@ -0,0 +1,12 @@
+// Print a bunny with a sign! Created 9/22/2024
+#include <iostream>
+int main(){
+  std::cout << "|￣￣￣￣￣￣￣￣|\n";
+  std::cout << "|   MESSAGE     |\n";
+  std::cout << "|     HERE      |\n";
+  std::cout << "|_______________|\n";
+  std::cout << "(\\__/) || \n";
+  std::cout << "(•ㅅ•) || \n";
+  std::cout << "/ 　 づ \n";
+}
+
