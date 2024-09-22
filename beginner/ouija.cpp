0e577087bd987b973504e0c660855b1539559b5e
hmkuklinski <hmkuklinski@gmail.com>
2024-09-22 03:51:13 -0400

    added ouija.cpp

---
+++ beginner/ouija.cpp
@@ -0,0 +1,15 @@
+// to print a Ouiji Board
+#include <iostream>
+int main(){
+  std::cout << "-----------------------------------\n";
+  std::cout << "|           YES   NO              |\n";
+  std::cout << "|                                 |\n";
+  std::cout << "|         ABCDEFGHIJKLM           |\n";
+  std::cout << "|         NOPQRSTUVWXYZ           |\n";
+  std::cout << "|                                 |\n";
+  std::cout << "|           1234567890            |\n";
+  std::cout << "|                                 |\n";
+  std::cout << "|       x HELLO  GOODBYE x        |\n";
+  std::cout << "-----------------------------------\n";
+}
+
