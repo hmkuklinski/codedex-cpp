cf3ae45e84f6f2cffbd13cd352800a77ab5bbe5b
hmkuklinski <hmkuklinski@gmail.com>
2024-09-22 04:45:52 -0400

    added knighthood.cpp

---
+++ beginner/knighthood.cpp
@@ -0,0 +1,6 @@
+// chaining variables and strings:
+#include <iostream>
+int main(){
+  std::string name = "Hannah";
+  std::cout << "In the name of the Warrior, I charge you to be brave.\nIn the name of the Father, I charge you to be just.\nIn the name of the Mother, I charge you to defend the innocent.\nArise, " <<name <<", a knight of the Seven Kingdoms.";
+}
