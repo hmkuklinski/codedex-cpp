b0441b471edbeffd3e08c33e5727815e39c5b10f
hmkuklinski <hmkuklinski@gmail.com>
2024-09-22 03:45:25 -0400

    added goodbye.cpp

---
+++ beginner/goodbye.cpp
@@ -0,0 +1,8 @@
+// prints out different ways to say goodbye in various languages
+#include <iostream>
+int main(){
+  std::cout << "Goodbye!\n";
+  std::cout << "잘 가요!\n";
+  std::cout << "じゃあね\n";
+  std::cout << "Adios\n";
+}
